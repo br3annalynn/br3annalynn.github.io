0d20de6eec27798d8b6c0043f747f99e3982ebce
Breanna <bturcsanyi@pillartechnology.com>
2015-05-25 18:13:26 -0400

    started vs

---
+++ GratefulHearts/GratefulHearts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GratefulHearts.Startup))]
+namespace GratefulHearts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
